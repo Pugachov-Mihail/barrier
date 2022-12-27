fe5459ebd4ac3205a6d006235b65bbade79a9aec
Pugachov-Mihail <xd383727@gmail.com>
2022-12-27 17:20:01 +0300

    fix

--- controllers/BarrierController.php
+++ controllers/BarrierController.php
@@ -19,6 +19,7 @@
                 if ($user->debt >= 350) {
                     echo "0; 0 - всё OK" . "</br> 1; $user->sender - должен $user->debt денег";
                 }else {
+                    
                     $e = escapeshellcmd('/home/admin/Desktop/backend/web/rele.py');
                     $command = exec($e);
                     echo "<br/>";
