1d7e1e958ac4a828bf11ff80db71fe184463f18c
xd383727@gmail.com <@!lodS9506>
2022-12-28 17:19:01 +0300

    create rex

--- controllers/BarrierController.php
+++ controllers/BarrierController.php
@@ -11,21 +11,30 @@
         }
         public function actionDebtor($number)
         {
-            $query = ListOfDebtors::find();
-            $user = $query
-                ->where('number = :number', [':number'=>$number])
-                ->one();
-            if ($user){
-                if ($user->debt >= 350) {
-                    echo "0; 0 - всё OK" . "</br> 1; $user->sender - должен $user->debt денег";
-                }else {
-                    $e = escapeshellcmd('/home/admin/Desktop/backend/web/rele.py');
-                    $command = exec($e);
-                    echo "<br/>";
-                    echo exec("a.php");
-                    print_r($command);
-                    //echo "0; 0 - всё OK" . $command;
+            if(preg_match("/[0-9]{0,11}/", $number)){
+                if(strlen($number)==11) {
+                    $query = ListOfDebtors::find();
+
+                    $user = $query
+                        ->where('number = :number', [':number' => $number])
+                        ->one();
+                    if ($user) {
+                        if ($user->debt >= 350) {
+                            echo "0; 0 - всё OK" . "</br> 1; $user->sender - должен $user->debt денег";
+                        } else {
+                            $e = escapeshellcmd('/home/admin/Desktop/backend/web/rele.py');
+                            $command = exec($e);
+                            echo "<br/>";
+                            echo exec("a.php");
+                            print_r($command);
+                            //echo "0; 0 - всё OK" . $command;
+                        }
+                    }
+                }else{
+                    echo "Короткий номер";
                 }
+            } else{
+                echo "Некорректный номер";
             }
         }
     }
